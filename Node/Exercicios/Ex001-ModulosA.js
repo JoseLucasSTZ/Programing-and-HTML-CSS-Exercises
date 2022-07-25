cc1eabf1f432f19e760a95d49e12e01421fbb18c
José Lucas Steinmetz da Silveira <jls.silveira@outlook.com>
2022-07-25 17:27:48 -0300

    update
    organization

--- Node/Exercicios/Ex001-ModulosA.js
+++
@@ -1,6 +0,0 @@
-this.ola = 'fala pessoal'
-exports.BemVindo='Bem vindo ao node'
-module.exports.atelogo='até o próximo exemplo'
-
-
-//obs: tanto THIS é um objeto, quanto exports, porque objetos em javascript são uma coleção dinâmica de pares cha/valor
--- Node/Exercicios/ex001-ModulosB.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    bomDia: 'Bom dia',
-    boaNoite(){
-        return 'Boa noite'
-    }
-}
-
-//versão mais clássica de trabalhar com exports
