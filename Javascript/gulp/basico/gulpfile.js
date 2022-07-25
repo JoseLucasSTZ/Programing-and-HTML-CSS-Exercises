5ac9c7e90b7bb3849987f522b754f212fdd5baac
José Lucas Steinmetz da Silveira <jls.silveira@outlook.com>
2022-07-25 17:25:52 -0300

    Update
    organization

--- Javascript/gulp/basico/gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp')
-const series = gulp.series // também pode ser desse jeito: const{ series } = require('gulp') 
-
-function copiar(cb) {
-    console.log('Tarefa de copiar!')
-    return cb()
-}
-
-module.exports.default = series(copiar) //o gulp espera o default
